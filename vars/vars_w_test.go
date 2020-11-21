224881868503c54cc115a9fd7cb36edd37d39827
Marko Kungla <marko.kungla@gmail.com>
2020-11-22 00:11:49 +0200

    Add TestValueFromString

--- vars/vars_w_test.go
+++ vars/vars_w_test.go
@@ -45,3 +45,24 @@ func TestParseFromBytes(t *testing.T) {
 		}
 	}
 }
+
+func TestValueFromString(t *testing.T) {
+	tests := []struct {
+		name string
+		val  string
+		want string
+	}{
+		{"STRING", "some-string", "some-string"},
+		{"STRING", "some-string with space ", "some-string with space"},
+		{"STRING", " some-string with space", "some-string with space"},
+		{"STRING", "1234567", "1234567"},
+	}
+	for _, tt := range tests {
+		if got := NewValue(tt.val); got.String() != tt.want {
+			t.Errorf("ValueFromString() = %q, want %q", got.String(), tt.want)
+		}
+		if rv := NewValue(tt.val); string(rv.Rune()) != tt.want {
+			t.Errorf("Value.Rune() = %q, want %q", string(rv.Rune()), tt.want)
+		}
+	}
+}
