ee5b8729a641ae73230e8d5c3e22838f1d495a65
Marko Kungla <marko.kungla@gmail.com>
2020-11-22 00:25:25 +0200

    separate value and collection tests

--- vars/collection_test.go
+++ vars/collection_test.go
@@ -5,23 +5,6 @@ import (
 	"testing"
 )
 
-func TestNewValue(t *testing.T) {
-	var tests = []struct {
-		val  interface{}
-		want string
-	}{
-		{nil, ""},
-		{"", ""},
-	}
-
-	for _, tt := range tests {
-		got := NewValue(tt.val).String()
-		if got != tt.want {
-			t.Errorf("want: %s got %s", tt.want, got)
-		}
-	}
-}
-
 func TestParseFromString(t *testing.T) {
 	slice := strings.Split(string(genStringTestBytes()), "\n")
 	collection := ParseKeyValSlice(slice)
@@ -42,27 +25,6 @@ func TestParseFromBytes(t *testing.T) {
 	for _, test := range stringTests {
 		if actual := collection.Get(test.key); actual.String() != test.want {
 			t.Errorf("Collection.Get(%q) = %q, want %q", test.key, actual.String(), test.want)
-		}
-	}
-}
-
-func TestValueFromString(t *testing.T) {
-	tests := []struct {
-		name string
-		val  string
-		want string
-	}{
-		{"STRING", "some-string", "some-string"},
-		{"STRING", "some-string with space ", "some-string with space"},
-		{"STRING", " some-string with space", "some-string with space"},
-		{"STRING", "1234567", "1234567"},
-	}
-	for _, tt := range tests {
-		if got := NewValue(tt.val); got.String() != tt.want {
-			t.Errorf("ValueFromString() = %q, want %q", got.String(), tt.want)
-		}
-		if rv := NewValue(tt.val); string(rv.Rune()) != tt.want {
-			t.Errorf("Value.Rune() = %q, want %q", string(rv.Rune()), tt.want)
 		}
 	}
 }
--- vars/vars_test.go
+++ vars/vars_test.go
@@ -1,6 +1,9 @@
 package vars
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
 type stringTest struct {
 	key  string
@@ -36,4 +39,42 @@ func genStringTestBytes() []byte {
 		out = append(out, []byte(line)...)
 	}
 	return out
+}
+
+func TestNewValue(t *testing.T) {
+	var tests = []struct {
+		val  interface{}
+		want string
+	}{
+		{nil, ""},
+		{"", ""},
+	}
+
+	for _, tt := range tests {
+		got := NewValue(tt.val).String()
+		if got != tt.want {
+			t.Errorf("want: %s got %s", tt.want, got)
+		}
+	}
+}
+
+func TestValueFromString(t *testing.T) {
+	tests := []struct {
+		name string
+		val  string
+		want string
+	}{
+		{"STRING", "some-string", "some-string"},
+		{"STRING", "some-string with space ", "some-string with space"},
+		{"STRING", " some-string with space", "some-string with space"},
+		{"STRING", "1234567", "1234567"},
+	}
+	for _, tt := range tests {
+		if got := NewValue(tt.val); got.String() != tt.want {
+			t.Errorf("ValueFromString() = %q, want %q", got.String(), tt.want)
+		}
+		if rv := NewValue(tt.val); string(rv.Rune()) != tt.want {
+			t.Errorf("Value.Rune() = %q, want %q", string(rv.Rune()), tt.want)
+		}
+	}
 }
