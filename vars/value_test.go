71fb4156012ca5f72ef1a0f379906aedd6fa1993
Marko Kungla <marko.kungla@gmail.com>
2020-11-22 02:11:52 +0200

    Add TestValueTypeAsInt64

--- vars/value_test.go
+++ vars/value_test.go
@@ -146,6 +146,23 @@ func TestValueTypeAsRune(t *testing.T) {
 		}
 	}
 }
+
+func TestValueTypeAsInt64(t *testing.T) {
+	tests := []struct {
+		val  Value
+		want int64
+	}{
+		{NewValue("1"), 1},
+		{NewValue("2"), 2},
+		{NewValue("4444447777777834555"), 4444447777777834555},
+	}
+	for _, tt := range tests {
+		got, _ := tt.val.Int(10, 0)
+		if got != int64(tt.want) {
+			t.Errorf("want: %d got %d", got, tt.want)
+		}
+	}
+}
 func TestValueTypeAsByte(t *testing.T) {
 	tests := []struct {
 		val  Value
