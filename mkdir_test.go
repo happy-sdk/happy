45d9f264aeee7f63aad9ceec1a0c78058d8ab7f6
Marko Kungla <marko.kungla@gmail.com>
2021-07-17 18:22:56 +0300

    update readme

--- mkdir_test.go
+++ mkdir_test.go
@@ -1,4 +1,4 @@
-// Copyright 2020 Marko Kungla. All rights reserved.
+// Copyright 2021 Marko Kungla. All rights reserved.
 // Use of this source code is governed by a license
 // that can be found in the LICENSE file.
 
