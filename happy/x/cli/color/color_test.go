c412984a71110eca7a1f092557b3650578189dc0
Marko Kungla <marko.kungla@gmail.com>
2022-12-16 18:57:35 +0200

    wip: update bexp dep
    Signed-off-by: Marko Kungla <marko.kungla@gmail.com>

--- happy/x/cli/color/color_test.go
+++ happy/x/cli/color/color_test.go
@@ -2,19 +2,28 @@ package color
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestColorFormatting(t *testing.T) {
-	assert.Equal(t, "\x1b[31mfoo\x1b[0m", Text("red", "foo"))
-	assert.Equal(t, "\x1b[32mfoo\x1b[0m", Text("green", "foo"))
-	assert.Equal(t, "\x1b[1;37mfoo\x1b[0m", Text("white", "foo"))
-	assert.Equal(t, "\x1b[0;37mfoo\x1b[0m", Text("gray", "foo"))
-	assert.Equal(t, "\x1b[0;30mfoo\x1b[0m", Text("darkgray", "foo"))
-	assert.Equal(t, "\x1b[33mfoo\x1b[0m", Text("yellow", "foo"))
-	assert.Equal(t, "\x1b[36mfoo\x1b[0m", Text("cyan", "foo"))
-	assert.Equal(t, "\x1b[34mfoo\x1b[0m", Text("blue", "foo"))
-	assert.Equal(t, "\x1b[30mfoo\x1b[0m", Text("black", "foo"))
-	assert.Equal(t, "\x1b[1;37mfoo\x1b[0m", Text("", "foo"))
+	var tests = []struct {
+		Color    string
+		Str      string
+		Expected string
+	}{
+		{"red", "foo", "\x1b[31mfoo\x1b[0m"},
+		{"green", "foo", "\x1b[32mfoo\x1b[0m"},
+		{"white", "foo", "\x1b[1;37mfoo\x1b[0m"},
+		{"gray", "foo", "\x1b[0;37mfoo\x1b[0m"},
+		{"darkgray", "foo", "\x1b[0;30mfoo\x1b[0m"},
+		{"yellow", "foo", "\x1b[33mfoo\x1b[0m"},
+		{"cyan", "foo", "\x1b[36mfoo\x1b[0m"},
+		{"blue", "foo", "\x1b[34mfoo\x1b[0m"},
+		{"black", "foo", "\x1b[30mfoo\x1b[0m"},
+		{"", "foo", "\x1b[1;37mfoo\x1b[0m"},
+	}
+	for _, test := range tests {
+		if res := Text(test.Color, test.Str); res != test.Expected {
+			t.Errorf("expected color string(%s) got(%s)", test.Expected, res)
+		}
+	}
 }
