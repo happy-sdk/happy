0cd16b15ae2bb401dfd1a307e4d19a7b7bc46cee
Marko Kungla <marko.kungla@gmail.com>
2023-01-13 16:19:02 +0200

    add usage comment for hello  example
    Signed-off-by: Marko Kungla <marko.kungla@gmail.com>

--- happy/examples/hello/hello.go
+++ happy/examples/hello/hello.go
@@ -19,6 +19,9 @@ import (
 func main() {
 	app := happy.New()
 	app.Do(func(sess *happy.Session, args happy.Args) error {
+		// name := args.Arg(0) // returns empty value if not present
+		// name, err := args.ArgDefault(0, "anonymous") // return vars.Value with default value "anonymous"
+		// Following is returning vars.Variable (vars.Variable can be directly logged)
 		name, err := args.ArgVarDefault(0, "name", "anonymous")
 		if err != nil {
 			return err
