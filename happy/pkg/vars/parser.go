c323eacf4297d773ecc84a0ae7fa68d1a4184cac
Marko Kungla <marko.kungla@gmail.com>
2023-02-02 21:57:45 +0200

    doc: update and clean vars docs
    Signed-off-by: Marko Kungla <marko.kungla@gmail.com>

--- happy/pkg/vars/parser.go
+++ happy/pkg/vars/parser.go
@@ -1,4 +1,4 @@
-// Copyright 2022 The Happy Authorsunicode/norm
+// Copyright 2022 Marko Kungla
 // Licensed under the Apache License, Version 2.0.
 // See the LICENSE file.
 
--- happy/pkg/vars/vars.go
+++ happy/pkg/vars/vars.go
@@ -1,10 +1,9 @@
-// Copyright 2022 The Happy Authors
+// Copyright 2022 Marko Kungla
 // Licensed under the Apache License, Version 2.0.
 // See the LICENSE file.
 
 // Package vars provides the API to parse variables from various input
-// formats/kinds to common key value pair.
-// Or key value pair sets to Variable collections.
+// formats/kinds to common key value pair. Or key value pair sets to Variable collections.
 package vars
 
 import (
