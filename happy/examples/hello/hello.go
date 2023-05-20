cabdf5e81a2e76764f6ac85251242a087523e62c
Marko Kungla <marko.kungla@gmail.com>
2023-05-20 20:16:46 +0300

    fix: update hello example help message using .Help
    Signed-off-by: Marko Kungla <marko.kungla@gmail.com>

--- happy/examples/hello/hello.go
+++ happy/examples/hello/hello.go
@@ -2,30 +2,26 @@ package main
 
 import (
 	"github.com/happy-sdk/happy"
-	"github.com/happy-sdk/happy/logging"
 	"golang.org/x/exp/slog"
 )
 
-// A simple hello application
-//
-// Usage:
-// - go run ./examples/hello/
-// - go run ./examples/hello/ nickname
-//
-// Increase verbosity
-// - go run ./examples/hello/ --debug
-// - go run ./examples/hello/ --system-debug
-//
-// Help
-// - go run ./examples/hello/ -h
 func main() {
-	loggerConf := logging.Config{}
-	logger, _ := logging.New(loggerConf)
+	app := happy.New()
 
-	app := happy.NewWithLogger(logger, logging.LevelDebug)
+	app.Help(`
+  A simple hello application
 
-	// func Clone[S ~[]E, E any](s S) S {
+  Usage:
+  - go run ./examples/hello/
+  - go run ./examples/hello/ nickname
 
+  Increase verbosity
+  - go run ./examples/hello/ --debug
+  - go run ./examples/hello/ --system-debug
+
+  Help
+  - go run ./examples/hello/ -h
+  `)
 	app.Do(func(sess *happy.Session, args happy.Args) error {
 		// name := args.Arg(0) // returns empty value if not present
 		// name, err := args.ArgDefault(0, "anonymous") // return vars.Value with default value "anonymous"
