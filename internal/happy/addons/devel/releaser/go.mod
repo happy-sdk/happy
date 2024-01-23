ac41e388f6080d689b688ee942e3a68ac1fe70b2
Marko Kungla <marko@mkungla.dev>
2024-01-23 19:28:14 +0200

    feat: clean internal
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- internal/happy/addons/devel/releaser/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/happy-sdk/happy/addons/devel/releaser
-
-go 1.21.5
--- internal/happy/x/cli/commands/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright 2022 The Happy Authors
-// Licensed under the Apache License, Version 2.0.
-// See the LICENSE file.
-
-// Package commands provides commonly used commands which you can simply plug into your application.
-package commands
