5fe299e72191bbdac08776d7686714b5ff4db00b
Marko Kungla <marko@mkungla.dev>
2024-01-29 21:54:25 +0200

    ci: add monorepo-coverage-windows
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- .github/actions/go-test-monorepo-action/go-test-monorepo-action.sh
+++ .github/actions/go-test-monorepo-action/go-test-monorepo-action.sh
@@ -8,13 +8,13 @@ modules=()
 
 # Find all directories containing a go.mod file
 while IFS= read -r module; do
-    modules+=("$module")
+  modules+=("$module")
 
-    # Run tests only if ONLY_MODULE_LIST is not "true"
-    if [ "$ONLY_MODULE_LIST" != "true" ]; then
-        echo "Testing and generating coverage for module: $module"
-        (cd "$module" && go test -race -coverpkg=./... -coverprofile=coverage.out -timeout=1m ./...)
-    fi
+  # Run tests only if ONLY_MODULE_LIST is not "true"
+  if [ "$ONLY_MODULE_LIST" != "true" ]; then
+    echo "Testing and generating coverage for module: $module"
+    (cd "$module" && go test -race -coverpkg=./... -coverprofile=coverage.out -timeout=1m ./...)
+  fi
 done < <(find . -type f -name 'go.mod' -exec dirname {} \;)
 
 # Convert modules array to JSON array format
@@ -22,7 +22,7 @@ modules_json=$(printf '%s\n' "${modules[@]}" | jq -R . | jq -cs .)
 
 # Check if GITHUB_OUTPUT is set, else use /dev/null
 if [ -z "$GITHUB_OUTPUT" ]; then
-    GITHUB_OUTPUT="/dev/null"
+  GITHUB_OUTPUT="/dev/null"
 fi
 
 # Output the modules for the matrix
@@ -30,7 +30,7 @@ echo "modules=$modules_json" >> $GITHUB_OUTPUT
 
 # If running in GitHub Actions, set info message
 if [ "$GITHUB_ACTIONS" == "true" ]; then
-    echo "::info::Monorepo modules: $modules_json"
+  echo "::info::Monorepo modules: $modules_json"
 else
-    echo "${modules_json}"
+  echo "${modules_json}"
 fi
