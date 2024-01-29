c2ff70079564d64e00527861755104bd1f1f078f
Marko Kungla <marko@mkungla.dev>
2024-01-29 20:54:48 +0200

    ci: list modules and pass it to test matrix
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- .github/actions/go-test-monorepo-action/go-test-monorepo-action.sh
+++ .github/actions/go-test-monorepo-action/go-test-monorepo-action.sh
@@ -1,21 +1,36 @@
 #!/bin/bash
 
+# Check if only module list should be outputted
+ONLY_MODULE_LIST="$1"
+
 # Array to hold module paths
 modules=()
 
 # Find all directories containing a go.mod file
 while IFS= read -r module; do
-  modules+=("$module")
-  echo "Testing and generating coverage for module: $module"
-  (cd "$module" && go test -race -coverpkg=./... -coverprofile=coverage.out -timeout=1m ./...)
+    modules+=("$module")
+
+    # Run tests only if ONLY_MODULE_LIST is not "true"
+    if [ "$ONLY_MODULE_LIST" != "true" ]; then
+        echo "Testing and generating coverage for module: $module"
+        (cd "$module" && go test -race -coverpkg=./... -coverprofile=coverage.out -timeout=1m ./...)
+    fi
 done < <(find . -type f -name 'go.mod' -exec dirname {} \;)
 
 # Convert modules array to JSON array format
 modules_json=$(printf '%s\n' "${modules[@]}" | jq -R . | jq -cs .)
 
+# Check if GITHUB_OUTPUT is set, else use /dev/null
 if [ -z "$GITHUB_OUTPUT" ]; then
-  GITHUB_OUTPUT="/dev/null"
+    GITHUB_OUTPUT="/dev/null"
 fi
+
 # Output the modules for the matrix
 echo "modules=$modules_json" >> $GITHUB_OUTPUT
-echo "${modules_json}"
+
+# If running in GitHub Actions, set info message
+if [ "$GITHUB_ACTIONS" == "true" ]; then
+    echo "::info::Monorepo modules: $modules_json"
+else
+    echo "${modules_json}"
+fi
