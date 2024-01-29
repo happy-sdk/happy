c37c6d15ac25c741f7305bccead203aa7d9ab5d8
Marko Kungla <marko@mkungla.dev>
2024-01-29 20:04:17 +0200

    ci: fix test action outputs json
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- .github/actions/go-test-monorepo-action/go-test-monorepo-action.sh
+++ .github/actions/go-test-monorepo-action/go-test-monorepo-action.sh
@@ -5,10 +5,17 @@ modules=()
 
 # Find all directories containing a go.mod file
 while IFS= read -r module; do
-    modules+=("$module")
-    echo "Testing and generating coverage for module: $module"
-    (cd "$module" && go test -race -coverpkg=./... -coverprofile=coverage.out -timeout=30s ./...)
+  modules+=("$module")
+  echo "Testing and generating coverage for module: $module"
+  (cd "$module" && go test -race -coverpkg=./... -coverprofile=coverage.out -timeout=1m ./...)
 done < <(find . -type f -name 'go.mod' -exec dirname {} \;)
 
+# Convert modules array to JSON array format
+modules_json=$(printf '%s\n' "${modules[@]}" | jq -R . | jq -s .)
+
+if [ -z "$GITHUB_OUTPUT" ]; then
+  GITHUB_OUTPUT="/dev/null"
+fi
 # Output the modules for the matrix
-echo "modules=$(IFS=,; echo "${modules[*]}")" >> $GITHUB_OUTPUT
+echo "modules=$modules_json" >> $GITHUB_OUTPUT
+echo "${modules_json}"
