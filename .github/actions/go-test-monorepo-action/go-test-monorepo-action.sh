15e01cd2494786525a18d536f15f5bf25dd7b3ae
Marko Kungla <marko@mkungla.dev>
2024-01-29 17:48:19 +0200

    ci: output covered modules
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

---
+++ .github/actions/go-test-monorepo-action/go-test-monorepo-action.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Array to hold module paths
+modules=()
+
+# Find all directories containing a go.mod file
+while IFS= read -r module; do
+    modules+=("$module")
+    echo "Testing and generating coverage for module: $module"
+    (cd "$module" && go test -race -coverpkg=./... -coverprofile=coverage.out ./...)
+done < <(find . -type f -name 'go.mod' -exec dirname {} \;)
+
+# Output the modules for the matrix
+echo "modules=$(IFS=,; echo "${modules[*]}")" >> $GITHUB_OUTPUT
--- .github/actions/golangci-lint-monorepo-action/golangci-lint-monorepo-action.sh
+++ .github/actions/golangci-lint-monorepo-action/golangci-lint-monorepo-action.sh
@@ -3,7 +3,7 @@
 # Determine the golangci-lint executable path
 GOLANGCI_LINT="golangci-lint"
 if [ -n "$GITHUB_WORKSPACE" ]; then
-    GOLANGCI_LINT="$GITHUB_WORKSPACE/bin/golangci-lint"
+  GOLANGCI_LINT="$GITHUB_WORKSPACE/bin/golangci-lint"
 fi
 
 # Find all directories containing a go.mod file
@@ -11,6 +11,6 @@ modules=$(find . -type f -name 'go.mod' -exec dirname {} \;)
 
 # Loop through each module and run golangci-lint
 for module in $modules; do
-    echo "Linting module: $module"
-    (cd "$module" && $GOLANGCI_LINT run ./...)
+  echo "Linting module: $module"
+  (cd "$module" && $GOLANGCI_LINT run ./...)
 done
