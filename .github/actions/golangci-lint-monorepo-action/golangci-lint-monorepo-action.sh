565f160c1bbb3562da691c0a7b7f980c697c33ec
Marko Kungla <marko@mkungla.dev>
2024-01-30 22:34:48 +0200

    ci: exit linter on error
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- .github/actions/golangci-lint-monorepo-action/golangci-lint-monorepo-action.sh
+++ .github/actions/golangci-lint-monorepo-action/golangci-lint-monorepo-action.sh
@@ -9,8 +9,20 @@ fi
 # Find all directories containing a go.mod file
 modules=$(find . -type f -name 'go.mod' -exec dirname {} \;)
 
+# Initialize a variable to track linting errors
+lint_errors=0
+
 # Loop through each module and run golangci-lint
 for module in $modules; do
   echo "Linting module: $module"
-  (cd "$module" && $GOLANGCI_LINT run ./...)
+  (cd "$module" && $GOLANGCI_LINT run ./...) || lint_errors=1
 done
+
+# Exit with 1 if there were linting errors
+if [ $lint_errors -ne 0 ]; then
+  echo "Linting errors found"
+  exit 1
+fi
+
+# Exit with 0 if everything was fine
+exit 0
