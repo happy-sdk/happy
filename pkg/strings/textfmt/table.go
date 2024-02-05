23f6da5b62b30f67c9eba7ddc0a1c10632db3458
Marko Kungla <marko@mkungla.dev>
2024-02-05 12:28:37 +0200

    feat: add text formating utils
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

---
+++ pkg/strings/textfmt/table.go
@@ -0,0 +1,118 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright © 2024 The Happy Authors
+
+// Package textfmt offers a collection of utils for plain text formatting.
+package textfmt
+
+import (
+	"strings"
+	"unicode/utf8"
+)
+
+type Table struct {
+	Title         string
+	WithHeader    bool
+	SkipEmptyRows bool
+	rows          [][]string
+	cols          int
+}
+
+func (t *Table) AddRow(cols ...string) {
+	if len(cols) == 0 && !t.SkipEmptyRows {
+		t.rows = append(t.rows, make([]string, t.cols))
+		return
+	}
+
+	t.rows = append(t.rows, cols)
+	if len(cols) > t.cols {
+		t.cols = len(cols)
+	}
+}
+
+func (t *Table) String() string {
+	if len(t.rows) == 0 || (t.WithHeader && len(t.rows) == 1) {
+		return ""
+	}
+	var b strings.Builder
+	maxColWidth, tableWidth := t.calculateMaxColumnWidths()
+
+	if t.Title != "" {
+		b.WriteString(t.buildBorder('┌', '─', '┐', maxColWidth))
+		suffixlen := tableWidth - utf8.RuneCountInString(t.Title)
+
+		suffix := ""
+		if suffixlen > 0 {
+			suffix = strings.Repeat(" ", suffixlen)
+		}
+		b.WriteString("│ " + t.Title + suffix + " │\n")
+		b.WriteString(t.buildBorder('├', '┬', '┤', maxColWidth))
+	} else {
+		b.WriteString(t.buildBorder('┌', '┬', '┐', maxColWidth))
+	}
+	for i, row := range t.rows {
+		b.WriteString(t.formatRow(row, maxColWidth))
+		if i == 0 && t.WithHeader {
+			b.WriteString(t.buildBorder('├', '┼', '┤', maxColWidth))
+		}
+	}
+
+	b.WriteString(t.buildBorder('└', '┴', '┘', maxColWidth))
+	b.WriteRune('\n')
+	return b.String()
+}
+
+func (t *Table) calculateMaxColumnWidths() (cw []int, total int) {
+	maxColWidth := make([]int, t.cols)
+
+	for _, row := range t.rows {
+		for i, col := range row {
+			colLen := len(col) + 2
+			if colLen > maxColWidth[i] {
+				maxColWidth[i] = colLen
+			}
+
+		}
+	}
+	total = -1
+	for _, w := range maxColWidth {
+		total += w
+	}
+	return maxColWidth, total
+}
+
+func (t *Table) buildBorder(left, middle, right rune, clens []int) string {
+	var b strings.Builder
+	b.WriteRune(left)
+	for i, l := range clens {
+		b.WriteString(strings.Repeat("─", l))
+		if i < len(clens)-1 {
+			b.WriteRune(middle)
+		}
+	}
+	b.WriteRune(right)
+	b.WriteRune('\n')
+	return b.String()
+}
+
+// ... [Other parts of your Table struct] ...
+
+func (t *Table) formatRow(row []string, colWidths []int) string {
+	var b strings.Builder
+	for i := 0; i < t.cols; i++ {
+		b.WriteRune('│')
+		col := ""
+		if i < len(row) {
+			col = row[i]
+		}
+		colDisplayWidth := utf8.RuneCountInString(col)
+		padding := colWidths[i] - colDisplayWidth - 1 // -1 for space before the text
+		if padding < 0 {
+			padding = 0
+		}
+		b.WriteString(" " + col + strings.Repeat(" ", padding))
+	}
+	b.WriteRune('│')
+	b.WriteRune('\n')
+	return b.String()
+}
