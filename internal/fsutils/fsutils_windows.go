186fe2225ecbdf4c421dc69b6c0bc82f7be93b0d
Marko Kungla <marko@mkungla.dev>
2024-03-10 18:14:57 +0200

    fix: windows fsutils GetDiskFreeSpaceEx call
    Signed-off-by: Marko Kungla <marko@mkungla.dev>

--- internal/fsutils/fsutils_windows.go
+++ internal/fsutils/fsutils_windows.go
@@ -7,27 +7,21 @@
 package fsutils
 
 import (
-	"unsafe"
-
 	"golang.org/x/sys/windows"
 )
 
 func AvailableSpace(path string) (uint64, error) {
-	lpFreeBytesAvailable := int64(0)
-	lpTotalNumberOfBytes := int64(0)
-	lpTotalNumberOfFreeBytes := int64(0)
+	lpFreeBytesAvailable := uint64(0)
+	lpTotalNumberOfBytes := uint64(0)
+	lpTotalNumberOfFreeBytes := uint64(0)
 
 	drive := windows.StringToUTF16Ptr(path)
 
-	ret, _, err := windows.GetDiskFreeSpaceEx.Call(
-		uintptr(unsafe.Pointer(drive)),
-		uintptr(unsafe.Pointer(&lpFreeBytesAvailable)),
-		uintptr(unsafe.Pointer(&lpTotalNumberOfBytes)),
-		uintptr(unsafe.Pointer(&lpTotalNumberOfFreeBytes)),
+	err := windows.GetDiskFreeSpaceEx(
+		drive,
+		&lpFreeBytesAvailable,
+		&lpTotalNumberOfBytes,
+		&lpTotalNumberOfFreeBytes,
 	)
-	if ret == 0 {
-		return 0, err
-	}
-
-	return uint64(lpFreeBytesAvailable), nil
+	return lpFreeBytesAvailable, err
 }
