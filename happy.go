8e7db46101e862446a40f5a168b9e2eb3a491605
Marko Kungla <marko.kungla@gmail.com>
2025-06-20 15:40:40 +0300

    wip: prepare release
    Signed-off-by: Marko Kungla <marko.kungla@gmail.com>

--- happy.go
+++ happy.go
@@ -18,6 +18,7 @@ import (
 	"errors"
 	"fmt"
 
+	_ "github.com/happy-sdk/happy/pkg/i18n"
 	"github.com/happy-sdk/happy/pkg/settings"
 	"github.com/happy-sdk/happy/pkg/strings/slug"
 	"github.com/happy-sdk/happy/sdk/app"
