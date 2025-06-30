19db8456e8bd45706c6d1174d0da10ee7684da2d
Marko Kungla <marko.kungla@gmail.com>
2025-06-30 21:19:47 +0300

    test: add tests for bug logging with slog and custom logger
    Signed-off-by: Marko Kungla <marko.kungla@gmail.com>

---
+++ pkg/devel/bug/bug_test.go
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: Apache-2.0
+//
+// Copyright © 2025 The Happy Authors
+
+package bug_test
+
+import (
+	"bytes"
+	"log/slog"
+	"strings"
+	"testing"
+
+	"github.com/happy-sdk/happy/pkg/devel/bug"
+	"github.com/happy-sdk/happy/pkg/logging"
+)
+
+func TestLogSlog(t *testing.T) {
+	var buf bytes.Buffer
+	logger := slog.New(slog.NewTextHandler(&buf, nil))
+	slog.SetDefault(logger)
+	bug.Log("test_bug")
+
+	out := buf.String()
+	if !strings.Contains(out, "test_bug") {
+		t.Errorf("expected out to contain %q, got %q", "test_bug\n", out)
+	}
+
+	if !strings.Contains(out, "level=ERROR+1") {
+		t.Errorf("expected out to contain %q, got %q", "level=ERROR+1\n", out)
+	}
+}
+
+func TestLogLogger(t *testing.T) {
+	var buf bytes.Buffer
+	logger := logging.NewDefaultWithWriter(&buf, logging.DefaultOptions())
+	slog.SetDefault(logger.Logger())
+
+	bug.Log("test_bug", logger)
+	out := buf.String()
+	if !strings.Contains(out, "test_bug") {
+		t.Errorf("expected out to contain %q, got %q", "test_bug\n", out)
+	}
+
+	if !strings.Contains(out, "level=bug") {
+		t.Errorf("expected out to contain %q, got %q", "level=bug\n", out)
+	}
+}
